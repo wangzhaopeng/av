d867fa661edb6da5a4f0f6d1828a0b858588cfe8
wzp <wzhaopeng@foxmail.com>
2017-10-24 15:18:12 +0800

    rtmp示例，发现srs服务器cpu占用过高

--- av/testrtmp.cpp
+++ av/testrtmp.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 #include "myrtmp.h"
 
+#include <windows.h>
+
 void wav_vec(int size, std::vector<vector<char>> &v_slice);
 
 void h264_vec(std::vector<vector<char>> &v_slice);
@@ -25,6 +27,7 @@ void tst_rtmp(void)
 	wav_vec(o_aac.get_input_samples(),va_slice);
 	double aac_slice_ms = (double)o_aac.get_input_samples()*1000/a_hz;
 	
+	cout << "tst_rtmp "<<endl;
 	myrtmp rtmp_sender;
 	bool bret = rtmp_sender.connect("rtmp://192.168.5.116/live/zb");
 	if (!bret){
@@ -36,26 +39,48 @@ void tst_rtmp(void)
 	////发送aac decoder info
 	rtmp_sender.init_a(o_aac.get_decoder_info());
 
-	unsigned int tick = 0;
-	for(int i = 2; i < (int)v_slice.size(); i++){  
-		int nalu_type = v_slice[i][0]&0x1F;
-		bool bKeyframe  = (nalu_type == 0x05) ? 1 : 0;
+	double h264_slice_ms = 1000/30.0;/////用的30帧的视频
 
-		rtmp_sender.send_v((char*)&v_slice[i][0],v_slice[i].size(),bKeyframe,tick);
-		//msleep(40);
-		tick += (int)aac_slice_ms;
+	int a_f = 0;
+	int v_f = 0;
+	DWORD ms_b = GetTickCount();
+	while(1){
+		if (GetTickCount()-ms_b>=a_f*h264_slice_ms){
+			int idx = a_f%(v_slice.size()-2)+2;/////2的作用是为了跳过开始的 sps pps
+			//int idx = a_f%(300)+2;/////每秒30帧，300帧过后又是I帧 感觉这样效果更好
+			int nalu_type = v_slice[idx][0]&0x1F;
+			bool bKeyframe  = (nalu_type == 0x05) ? 1 : 0;
+			DWORD ms_bsendv = GetTickCount();
+			bret = rtmp_sender.send_v((char*)&v_slice[idx][0],v_slice[idx].size(),bKeyframe,GetTickCount());
+			if (!bret){
+				cout<<"rtmp_sender.send_v false"<<endl;
+			}
+			//cout << "rtmp_sender.send_v ms "<<GetTickCount()-ms_bsendv<<" size "<<v_slice[idx].size() <<endl;
+			a_f++;
+		}
 
-		{
+		if (GetTickCount()-ms_b>=v_f*aac_slice_ms){
+			int idx = v_f%(va_slice.size());
 			vector<char> v_aac;
-			static int aac_tick = 0;
-			o_aac.pcm2aac(&va_slice[i][0],o_aac.get_input_samples(),v_aac);
+			DWORD ms_pcm2aac = GetTickCount();
+			o_aac.pcm2aac(&va_slice[idx][0],o_aac.get_input_samples(),v_aac);
+			//cout << "o_aac.pcm2aac ms "<<GetTickCount()-ms_pcm2aac<<" size "<<va_slice[idx].size() <<endl;
 			if (v_aac.size()>0){
-				rtmp_sender.send_a((&v_aac[0])+7,v_aac.size()-7,aac_tick);
-				aac_tick += (int)aac_slice_ms;//aac每次1024个采样，在22050hz下需要46ms，上面帧率跟这个也有关
+				DWORD ms_bsenda = GetTickCount();
+				bret = rtmp_sender.send_a((&v_aac[0])+7,v_aac.size()-7,GetTickCount());
+				if (!bret){
+					cout<<"rtmp_sender.send_a false"<<endl;
+				}
+				//cout << "rtmp_sender.send_a ms "<<GetTickCount()-ms_bsenda<<" size "<<v_aac.size() <<endl;
+			}
+			v_f++;
+
+			if (v_f>=(int)va_slice.size()){//////是否无限循环
+				break;
 			}
 		}
-	}  
+		Sleep(1);
+	}
 
-int a = 0;
-a++;
+	return;
 }
