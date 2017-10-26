18dac8d180471ba4095cef3f21abc62fb74d9b8c
wzp <wzhaopeng@foxmail.com>
2017-10-26 16:43:04 +0800

    update .gitignore

--- av/testrtmp.cpp
+++ av/testrtmp.cpp
@@ -1,4 +1,6 @@
 
+#include <sstream>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
@@ -14,6 +16,42 @@ void wav_vec(int size, std::vector<vector<char>> &v_slice);
 
 void h264_vec(std::vector<vector<char>> &v_slice);
 
+
+static string hex2str(const char*p,int size)
+{
+	ostringstream oss;
+	oss << "size "<<size<<":\t\t";
+	string str_hex;
+
+	str_hex += oss.str();
+
+	const int buf_size = size*8;
+	char *char_buf = new char[buf_size+8]();
+	int idx = 0;
+	for(int i = 0; i < size; i++){
+		//idx += sprintf(char_buf+idx,"0x%02x,",(unsigned char)*(p+i));/////有警告
+		//idx += sprintf_s(char_buf+idx,buf_size-idx,"0x%02x,",(unsigned char)*(p+i));/////太慢
+		unsigned char tem_v = (unsigned char)*(p+i);
+		char_buf[idx++] = '0';
+		char_buf[idx++] = 'x';
+		if ((tem_v>>4)<10){
+			char_buf[idx++] = (tem_v>>4)+'0';
+		}else{
+			char_buf[idx++] = (tem_v>>4)-10+'a';
+		}
+		if ((tem_v&0xf)<10){
+			char_buf[idx++] = (tem_v&0xf)+'0';
+		}else{
+			char_buf[idx++] = (tem_v&0xf)-10+'a';
+		}
+		
+		char_buf[idx++] = ',';
+	}
+
+	str_hex = str_hex+char_buf+"\n";
+	delete[] char_buf;
+	return str_hex;
+}
 
 static void tst_send(void)
 {
@@ -41,6 +79,8 @@ static void tst_send(void)
 
 	double h264_slice_ms = 1000/30.0;/////用的30帧的视频
 
+	string str_send_hex;
+
 	int a_f = 0;
 	int v_f = 0;
 	DWORD ms_b = GetTickCount();
@@ -51,12 +91,18 @@ static void tst_send(void)
 			int nalu_type = v_slice[idx][0]&0x1F;
 			bool bKeyframe  = (nalu_type == 0x05) ? 1 : 0;
 			DWORD ms_bsendv = GetTickCount();
-			bret = rtmp_sender.send_v((char*)&v_slice[idx][0],v_slice[idx].size(),bKeyframe,GetTickCount());
+			bret = rtmp_sender.send_v(&v_slice[idx][0],v_slice[idx].size(),bKeyframe,GetTickCount());
 			if (!bret){
 				cout<<"rtmp_sender.send_v false"<<endl;
 			}
-			//cout << "rtmp_sender.send_v ms "<<GetTickCount()-ms_bsendv<<" size "<<v_slice[idx].size() <<endl;
+			cout << "rtmp_sender.send_v ms "<<GetTickCount()-ms_bsendv<<" size "<<v_slice[idx].size() <<endl;
+
+			string tem_str = hex2str(&v_slice[idx][0],v_slice[idx].size());
+
 			a_f++;
+			if (a_f>=(int)v_slice.size()){//////是否无限循环
+				break;
+			}
 		}
 
 		if (GetTickCount()-ms_b>=v_f*aac_slice_ms){
@@ -71,15 +117,24 @@ static void tst_send(void)
 				if (!bret){
 					cout<<"rtmp_sender.send_a false"<<endl;
 				}
-				//cout << "rtmp_sender.send_a ms "<<GetTickCount()-ms_bsenda<<" size "<<v_aac.size() <<endl;
-			}
-			v_f++;
+				cout << "rtmp_sender.send_a ms "<<GetTickCount()-ms_bsenda<<" size "<<v_aac.size() <<endl;
 
+				string tem_str = hex2str((&v_aac[0])+7,v_aac.size()-7);
+			}
+
+			v_f++;
 			if (v_f>=(int)va_slice.size()){//////是否无限循环
 				break;
 			}
 		}
 		Sleep(1);
+	}
+
+	{
+		ofstream ofile;
+		ofile.open("send_rtmp_hex.txt");
+		ofile<<str_send_hex;
+		ofile.close();
 	}
 
 	return;
@@ -106,7 +161,9 @@ static void tst_rcv(void)
 		fclose(fp);
 		return;
 	}
-	
+
+	string str_rcv_hex;
+
 	const int buf_size = 1024*1024*5;
 	char *p_buf = new char[buf_size];
 	while(1){
@@ -115,14 +172,21 @@ static void tst_rcv(void)
 		if (r_size == 0){
 			break;
 		}
+		cout << "rcv size "<<r_size<<endl;
 		fwrite(p_buf,1,r_size,fp);
+		str_rcv_hex += hex2str(p_buf,r_size);
+	}
+	delete[] p_buf;
+	fclose(fp);
+	
+	{
+		ofstream ofile;
+		ofile.open("rcv_rtmp_hex.txt");
+		ofile<<str_rcv_hex;
+		ofile.close();
 	}
 
-
-	delete[] p_buf;
-
-
-	fclose(fp);
+	return;
 }
 
 void tst_rtmp(void)
